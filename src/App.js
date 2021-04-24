70343af1b23d45d87447fb99315c202ec744d1e1
Guru Charan <70446767+guruc-134@users.noreply.github.com>
2021-04-24 11:37:45 +0530

    added redirect so that the user will not be seeing the signin page once already signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch ,Route}  from 'react-router-dom';
+import { Switch ,Route,Redirect}  from 'react-router-dom';
 import {connect} from 'react-redux';
 import HomePage from './pages/homepage/homepage.component';
  import ShopPage from './pages/shop/shop.component';
@@ -27,6 +27,7 @@ class App extends React.Component {
                 }
               )
               // console.log(this.state)
+
           });      
       } 
       else
@@ -46,13 +47,19 @@ class App extends React.Component {
       <Switch>
         <Route exact path='/' component = {HomePage} />
         <Route exact path='/shop' component = {ShopPage}/>
-        <Route exact path='/signin' component = {SignInAndSignupPage}/>
+        <Route exact path='/signin' render = {() =>this.props.currentUser?
+        (<Redirect to = '/'/>)
+        :
+        (<SignInAndSignupPage/>) }
+        />
       </Switch>
     </div>
   );}
 }
-
+const mapStateToProps = ({user}) => ({
+  currentUser : user.currentUser
+})
 const mapDispatchToProps = dispatch =>({
   setCurrentUser : user =>dispatch(setCurrentUser(user))
 })
-export default connect(null,mapDispatchToProps)(App);
+export default connect(mapStateToProps,mapDispatchToProps)(App);
